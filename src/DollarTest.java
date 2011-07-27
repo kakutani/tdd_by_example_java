2bb26d823bdf7126d48c2a1e0258b0f28aa73578
Kakutani Shintaro <shintaro@kakutani.com>
2011-07-27 10:05:13 +0900

    chpater2, p.13

--- src/DollarTest.java
+++ src/DollarTest.java
@@ -4,8 +4,10 @@ import junit.framework.TestCase;
 public class DollarTest extends TestCase {
 	public void testMulitplication() {
 		Dollar five = new Dollar(5);
-		five.times(2);
-		assertEquals(10, five.amount);
+		Dollar product = five.times(2);
+		assertEquals(10, product.amount);
+		product = five.times(3);
+		assertEquals(15, product.amount);
 	}
 
 }
