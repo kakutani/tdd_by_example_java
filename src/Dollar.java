0ee8aaa7c4a5fad53fdf86b3f8e7d680ed55a588
Kakutani Shintaro <shintaro@kakutani.com>
2011-08-08 12:21:00 +0900

    remove unused private variable.

--- src/Dollar.java
+++ src/Dollar.java
@@ -1,5 +1,4 @@
 public class Dollar extends Money {
-	private String currency;
 
 	Dollar(int amount, String currency) {
 		super(amount, currency);
--- src/Franc.java
+++ src/Franc.java
@@ -1,7 +1,5 @@
 
 public class Franc extends Money {
-	private String currency;
-
 	Franc(int amount, String currency) {
 		super(amount, currency);
 	}
