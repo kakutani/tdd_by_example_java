a27b03255dcb71e7bcee207507b33ada12175a8e
Kakutani Shintaro <shintaro@kakutani.com>
2011-08-11 01:35:25 +0900

    chpater 12(wip).

--- src/Money.java
+++ src/Money.java
@@ -32,4 +32,8 @@ class Money {
 	public String toString() {
 		return amount + " " + currency;
 	}
+
+	public Money plus(Money addend) {
+		return new Money(amount + addend.amount, currency);
+	}
 }
--- src/MoneyTest.java
+++ src/MoneyTest.java
@@ -18,4 +18,10 @@ public class MoneyTest extends TestCase {
 		assertEquals("USD", Money.dollar(1).currency());
 		assertEquals("CHF", Money.franc(1).currency());
 	}
+	
+	public void testSimpleAddition() {
+		Money sum = Money.dollar(5).plus(Money.dollar(5));
+		assertEquals(Money.dollar(10), sum);
+				
+	}
 }
